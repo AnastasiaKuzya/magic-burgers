968c53dda5955331b9572b2d8f5e96f195c13837
Anastasia <ananastya.kuzya@gmail.com>
2021-05-29 22:50:35 +0300

    Fixed mistakes after soft review

--- src/components/BurgerConstructor/BurgerConstructor.tsx
+++ src/components/BurgerConstructor/BurgerConstructor.tsx
@@ -14,30 +14,48 @@ type TBurgerConstructorProps = {
 
 const BurgerConstructor = ({ className, ingredients }: TBurgerConstructorProps) => {
   const [price] = React.useState<number>(ingredients.reduce((sum, current) => sum + current.price, 0));
-
+  const bun = ingredients.find(i => i.type === 'bun') || ingredients[0];
   return (
     <section className={classNames(styles.host, className)}>
-      <ul className={`${styles.list} mb-10 pl-10`} style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
-        {ingredients.map((i) => (
-          <li key={`constructor-${i._id}`} className={`${styles.item} pl-8`}>
-            {i.type !== 'bun' && (
+      <ul className={`mb-10 pl-10`} style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+        <li key={`constructor-${bun._id}`} className={`${styles.item} pl-8`}>
+          <ConstructorElement
+            type='top'
+            isLocked={true}
+            text={bun.name}
+            price={bun.price}
+            thumbnail={bun.image_mobile}
+          />
+        </li>
+        <ul className={styles.list} style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+          {ingredients.filter(i => i.type !== 'bun').map((i) => (
+            <li key={`constructor-${i._id}`} className={`${styles.item} pl-8`}>
               <span className={styles.icon}>
-                <DragIcon type="primary" />
+                <DragIcon type='primary' />
               </span>
-            )}
-            <ConstructorElement
-              type={i.type === 'bun' ? 'top' : undefined}
-              isLocked={false}
-              text={i.name}
-              price={i.price}
-              thumbnail={i.image_mobile}
-            />
-          </li>
-        ))}
+              <ConstructorElement
+                type={undefined}
+                isLocked={false}
+                text={i.name}
+                price={i.price}
+                thumbnail={i.image_mobile}
+              />
+            </li>
+          ))}
+        </ul>
+        <li key={`constructor-${bun._id}`} className={`${styles.item} pl-8`}>
+          <ConstructorElement
+            type='bottom'
+            isLocked={true}
+            text={bun.name}
+            price={bun.price}
+            thumbnail={bun.image_mobile}
+          />
+        </li>
       </ul>
       <div className={`${styles.footer} mt-10 pr-10`}>
-        <Price className="mr-10" price={price} size="medium" />
-        <Button type="primary" size="large">
+        <Price className='mr-10' price={price} size='medium' />
+        <Button type='primary' size='large'>
           Нажми на меня
         </Button>
       </div>
